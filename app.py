193f7318a43242bc8533844a9e058f45c1a8ac38
Abishek Bhat <bhat.abishek@gmail.com>
2013-05-25 18:24:26 +0530

    Now calls are submitted to a Redis Queue to better handle the outgoing requests

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
-from flask import Flask,request,Response,render_template
-import plivo
+from flask import Flask,request,Response,render_template,redirect,url_for,make_response
 import json
+import plivo_helper
+from models import group
+from redis import Redis
+from rq import Queue
 app=Flask(__name__)
-BASE_URL="127.0.0.1:5000"
+app.debug=True
 
-def make_call(number,message): 
-	auth_id = ""
-	auth_token = ""
-
-	p = plivo.RestAPI(auth_id, auth_token)
-
-	params = {
-    	'from': '1212121212', 
-    	'to': number,
-    	'answer_url' : BASE_URL+"/answer?"+message,
-    	'answer_method' : "GET",
-	}
-	response=p.make_call(params)
-	print response
-	return response
+job_queue=Queue(connection=Redis())
 
 @app.route('/')
 def index():
 	return render_template('index.html') 
 
 
-@app.route('/call')
+@app.route('/call',methods=['POST'])
 def call():
-	number=request.args.get('number')
-	message=request.args.get('message')
-	print message
-	response=make_call(number,message)
-	return Response(json.dumps(response),mimetype='application/json')
+
+	message=request.form['message']
+	numbers=[int(request.form['number'+str(i)])for i in range(1,6)]
+	group.create_group(message,numbers)
+	for number in numbers:
+		job_queue.enqueue(plivo_helper.call,number,message)
+	return "Successfully submitted to the queue"
+
+		
+
 @app.route('/answer')
 def answer():
 	message=request.args.get('message')
-	plivo_response=plivo.Response()
-	plivo_response.addSpeak(message,loop=1)
-	return render_template('response.xml',response=plivo_response)
+	response=make_response(plivo_helper.add_message(message))
+	response.headers['Content-Type']='text/xml'
+	return response
+
 if __name__=='__main__':
 	app.run()
